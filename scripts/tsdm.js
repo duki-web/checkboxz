e98307807f8bd3884385c73a99af61bc72bbcdc3
duki-web <68391940+duki-web@users.noreply.github.com>
2023-06-22 13:29:44 +0800

    Update tsdm.js

--- scripts/tsdm.js
+++ scripts/tsdm.js
@@ -1,20 +1,20 @@
 const rules = {
     name: "【天使动漫】： ",
     cookie: config.tsdm.cookie,
-    url: "https://www.tsdm39.net/plugin.php?id=dsu_paulsign:sign&mobile=yes", //用于获取formhash的链接     
+    url: "https://www.tsdm39.com/plugin.php?id=dsu_paulsign:sign&mobile=yes", //用于获取formhash的链接     
     formhash: 'formhash=(.+?)\&', //formhash正则
     verify: "您需要先登录才能继续本操作", //验证cookie状态
     op: [{
             name: "签到",
             method: "post",
-            url: "https://www.tsdm39.net/plugin.php?id=dsu_paulsign:sign&operation=qiandao&infloat=0&inajax=0&mobile=yes", //签到链接
+            url: "https://www.tsdm39.com/plugin.php?id=dsu_paulsign:sign&operation=qiandao&infloat=0&inajax=0&mobile=yes", //签到链接
             data: "formhash=@formhash&qdxq=kx&qdmode=3&todaysay=&fastreply=1"
         },
         {
             name: "打工",
             ua: "pc",
             method: "post", 
-            url: "https://www.tsdm39.net/plugin.php?id=np_cliworkdz:work",
+            url: "https://www.tsdm39.com/plugin.php?id=np_cliworkdz:work",
             data: "act=getcre"
         }]
 };
